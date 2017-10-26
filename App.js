0811c36b0f6230aef9890f8006e6be92499436cf
entcheva <christina.entcheva@gmail.com>
2017-10-26 13:40:49 -0400

    Add LoginForm to App, add .gitignore

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './src/components/common';
+import LoginForm from './src/components/LoginForm';
 
 export default class App extends Component {
   componentWillMount() {
@@ -19,7 +20,7 @@ export default class App extends Component {
     return (
     <View>
       <Header headerText='Authentication' />
-      <Text>hi!</Text>
+      <LoginForm />
     </View>
     );
   }
